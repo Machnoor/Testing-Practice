ded5a9e574e0716a9546f2819223e575c9791ffb
Kabir <achalamallikarjun@gmail.com>
2023-01-27 18:05:51 +0100

    First Upload

---
+++ JavaBasics/src/Testing/JavaOperators.java
@@ -0,0 +1,15 @@
+package Testing;
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=23;
+		int s = a + b;
+		System.out.println(s);
+		
+		
+
+	}
+
+}
