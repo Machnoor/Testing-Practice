e532c542da412df476d5be3f776b3ed21a4161e4
Kabir <achalamallikarjun@gmail.com>
2023-02-05 11:49:59 +0100

    Arrays

--- JavaBasics/src/Testing/Strings.java
+++ JavaBasics/src/Testing/Strings.java
@@ -6,9 +6,24 @@ public class Strings {
 		// TODO Auto-generated method stub
 	
 		String str= "Then he said, \"Hello, World!\"";
-		System.out.println(str);
+		String str1 = " to \"Automation\"";
+		String str3 = "This ,is, Automation, Testing"; 
+		 String str4 = "The quick brown fox jumps over the lazy dog.'";
+	str4 = str4.concat("Is it true");
+		System.out.println("Length of the string is :" + str3.length());
+		System.out.println("Change to lowercase  :" + str3.toLowerCase());
+		System.out.println("Change to uppercase  :" + str3.toUpperCase());
+		System.out.println(str3.indexOf("is"));
+		System.out.println(str3.replace('i','o'));
+		System.out.println(str4.endsWith("dog.'"));
+		//String words = str4.split())
+		//console.log(words[3]);
 		
-		int Amt = 1000;
+		
+		//System.out.println(str);
+		//System.out.println(str1);
+		
+		/*int Amt = 1000;
 		double rate = 0.05; 
 		int duration=3;
 	    double Total = Amt*(1+rate*duration);
@@ -23,7 +38,7 @@ public class Strings {
 				
 	String str3 ="AutomationTesting";
 	System.out.println(str3.toLowerCase());
-	System.out.println(str3.toUpperCase());
+	System.out.println(str3.toUpperCase());*/
 	
 
 	}
